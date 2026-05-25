b46c7fc67c0c4c9100f92a35a1f7cac55369abfb
cgarcstudio <cgarcstudio@gmail.com>
2026-05-25 18:24:48 +0200

    Create products-db.js

---
+++ products-db.js
@@ -0,0 +1,39 @@
+// دیتابیس مرکزی محصولات - برای اضافه کردن محصول جدید فقط یک بلاک به این لیست اضافه کنید
+const projectsData = [
+    {
+        "title": "LEICA Q3",
+        "type": "FULL TUTORIAL",
+        "cover": "public/image/game/covers/leica_q3_cover.webp",
+        "url": "game_pages/leica_q3.html"
+    },
+    {
+        "title": "COMBAT DRONE",
+        "type": "FULL TUTORIAL",
+        "cover": "public/image/game/covers/combat_drone_cover.webp",
+        "url": "game_pages/combat_drone.html"
+    },
+    {
+        "title": "AXES VOL.02",
+        "type": "GAME READY BASE MESH",
+        "cover": "public/image/game/covers/axes_cover_vol02.webp",
+        "url": "game_pages/axes_v2.html"
+    },
+    {
+        "title": "AXES VOL.01",
+        "type": "GAME READY BASE MESH",
+        "cover": "public/image/game/covers/axes_cover_vol01.webp",
+        "url": "game_pages/axes_v1.html"
+    },
+    {
+        "title": "DAGGERS VOL.02",
+        "type": "GAME READY",
+        "cover": "public/image/game/covers/dagger_cover_vol02.webp",
+        "url": "game_pages/dagger_v2.html"
+    },
+    {
+        "title": "DAGGERS VOL.01",
+        "type": "GAME READY",
+        "cover": "public/image/game/covers/dagger_cover_vol01.webp",
+        "url": "game_pages/daggers_v1.html"
+    }
+];
