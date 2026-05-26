0e253a528fbf6b9e40fb0cc4215c2572e37b46c2
cgarcstudio <cgarcstudio@gmail.com>
2026-05-26 09:44:54 +0200

    Update components.js

--- js/components.js
+++ js/components.js
@@ -1,16 +1,15 @@
 // Component: Generates Project Card HTML dynamically
 function createCard(project) {
-    const isGameArt = project.category === 'gameart';
-    const glowClass = isGameArt ? 'game-art-card' : 'arch-viz-card';
-    
     return `
-        <a href="${project.url}" class="card card-link ${glowClass}">
-            <div class="card-img-holder">
-                <img src="${project.image}" alt="${project.title}">
-            </div>
-            <div class="card-info">
-                <span class="card-title">${project.title}</span>
-                <span class="card-category">${project.type}</span>
+        <a href="${project.url}" class="card-link">
+            <div class="card">
+                <div class="card-img-holder">
+                    <img src="${project.image}" alt="${project.title}">
+                </div>
+                <div class="card-info">
+                    <span class="card-title">${project.title}</span>
+                    <span class="card-category">${project.type}</span>
+                </div>
             </div>
         </a>
     `;
