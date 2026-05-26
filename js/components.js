a51ede90b17b404c1c7fba0bc2b6a13e4ea29270
cgarcstudio <cgarcstudio@gmail.com>
2026-05-26 09:02:09 +0200

    Update components.js

--- js/components.js
+++ js/components.js
@@ -4,13 +4,13 @@ function createCard(project) {
     const glowClass = isGameArt ? 'game-art-card' : 'arch-viz-card';
     
     return `
-        <a href="${project.link}?id=${project.id}" class="card ${glowClass}">
-            <div class="card-image" style="background-image: url('${project.image}');"></div>
+        <a href="${project.url}" class="card card-link ${glowClass}">
+            <div class="card-img-holder">
+                <img src="${project.image}" alt="${project.title}">
+            </div>
             <div class="card-info">
-                <h3 style="font-size: 1.2rem; color: #ffffff;">${project.title}</h3>
-                <div class="card-details">
-                    <p style="color: #5c6470; font-size: 0.9rem; line-height: 1.4;">${project.description}</p>
-                </div>
+                <span class="card-title">${project.title}</span>
+                <span class="card-category">${project.type}</span>
             </div>
         </a>
     `;
